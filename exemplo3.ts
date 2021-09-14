6737f53a516b1f4c35982908608b800c3a4d0ceb
Joao Carlos Morais <jcmorais.araujo@gmail.com>
2021-09-14 14:14:45 -0300

    orientacao a objetoo com TS

---
+++ exemplo3.ts
@@ -0,0 +1,30 @@
+class Carro {
+
+    private modelo : string
+    private numeroDePortas : number
+    private velocidade : number = 0
+
+    constructor(modelo : string, numeroDePortas : number){
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+
+    public acelerar() : void {
+        this.velocidade +=10;
+    }
+
+    public parar() : void {
+        this.velocidade = 0;
+    }
+
+    public velocidadeAtual() : number {
+        return this.velocidade;
+    }
+    
+};
+
+let carroA = new Carro("Veloster", 3);
+
+carroA.acelerar();
+
+
